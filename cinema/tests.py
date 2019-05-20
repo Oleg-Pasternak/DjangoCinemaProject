b7a05e3e1591c60c900fc216537ed0236fec7934
fr33zys <freezys@protonmail.com>
2019-05-20 19:48:19 +0100

    Fixed mobile version

--- cinema/tests.py
+++ cinema/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from account.models import Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestProfile(TestCase):
+    @classmethod
+    def setUp(self):
+        self.user = User.objects.create_user(username='jacob',
+                                             email='test@example.com',
+                                             password='top_secret')
+        self.first_name = "Jacob"
+        self.last_name = "Smeeth"
+
+    def test_profile_creation(self):
+        object = Profile.objects.create(user=self.user,
+                                        first_name=self.first_name,
+                                        last_name=self.last_name)
+        # check if object was created (if exists)
+        self.assertTrue(object)
