79d67f92d8a47f9a3741f72afde60abd245e9282
fr33zys <freezys@protonmail.com>
2019-05-25 11:06:34 +0100

    Fixed mobile version

--- cinema/tests.py
+++ cinema/tests.py
@@ -7,14 +7,16 @@ class TestProfile(TestCase):
     @classmethod
     def setUp(self):
         self.user = User.objects.create_user(username='jacob',
-                                             email='test@example.com',
+                                             image='home/web/Downloads/poster-rick-and-morty.jpg',
                                              password='top_secret')
         self.first_name = "Jacob"
         self.last_name = "Smeeth"
+        self.image = 'home/web/Downloads/poster-rick-and-morty.jpg'
 
     def test_profile_creation(self):
         object = Profile.objects.create(user=self.user,
                                         first_name=self.first_name,
-                                        last_name=self.last_name)
+                                        last_name=self.last_name,
+                                        image=self.image)
         # check if object was created (if exists)
         self.assertTrue(object)
